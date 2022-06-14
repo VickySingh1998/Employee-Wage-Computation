6f218c402bfa498ef40e147fcd6223aaee44a4df
VickySingh1998 <104664359+VickySingh1998@users.noreply.github.com>
2022-06-15 00:19:03 +0530

    added: new statement to find monthly wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,25 +3,32 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int EMPLOYEE_RATE_PER_HOUR = 20;
+    public static final int NUMBER_OF_WORKING_DAYS = 20;
     public static void main(String[] ags) {
 
-        int empHours = 0;
-        int empWage = 0;
+        int employeeHours = 0;
+        int employeeWage = 0;
+        int totalEmployeeWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
+        for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (employeeCheck) {
 
-            case (IS_FULL_TIME):
-                empHours = 8;
-                break;
-            case (IS_PART_TIME):
-                empHours = 4;
-                break;
-            default:
-                empHours = 0;
+                case (IS_FULL_TIME):
+                    employeeHours = 8;
+                    break;
+                case (IS_PART_TIME):
+                    employeeHours = 4;
+                    break;
+                default:
+                    employeeHours = 0;
+            }
+            employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
+            totalEmployeeWage += employeeWage;
+            System.out.println("Daily employee wage : " + employeeWage);
         }
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("Daily employee wage : "+empWage);
+        System.out.println();
+        System.out.println("Total employee wage : " +totalEmployeeWage );
     }
 }

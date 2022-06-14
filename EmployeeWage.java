b596d39430e9cd91b26bad2768e46858835e7c3b
VickySingh1998 <104664359+VickySingh1998@users.noreply.github.com>
2022-06-15 01:03:48 +0530

    modified: to get the total wage till a condition is reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,16 +5,17 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME = 2;
     public static final int EMPLOYEE_RATE_PER_HOUR = 20;
     public static final int NUMBER_OF_WORKING_DAYS = 20;
+    public static final int MAX_HOURS_A_MONTH = 100;
     public static void main(String[] ags) {
 
         int employeeHours = 0;
-        int employeeWage = 0;
-        int totalEmployeeWage = 0;
+        int totalWorkingDays = 0, totalEmployeeHours=0;
 
-        for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+        while (totalEmployeeHours <= MAX_HOURS_A_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (employeeCheck) {
 
+            switch (employeeCheck) {
                 case (IS_FULL_TIME):
                     employeeHours = 8;
                     break;
@@ -24,10 +25,11 @@ public class EmployeeWage {
                 default:
                     employeeHours = 0;
             }
-            employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
-            totalEmployeeWage += employeeWage;
-            System.out.println("Daily employee wage : " + employeeWage);
+            totalEmployeeHours += employeeHours;
+            System.out.println("Day " + totalWorkingDays +" Employee working Hour: "+employeeHours);
+
         }
+        int totalEmployeeWage = totalEmployeeHours * EMPLOYEE_RATE_PER_HOUR;
         System.out.println();
         System.out.println("Total employee wage : " +totalEmployeeWage );
     }

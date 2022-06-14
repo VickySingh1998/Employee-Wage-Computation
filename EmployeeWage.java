15919927f7ced638d9fd5c04cbe2a13f37d8ec99
VickySingh1998 <104664359+VickySingh1998@users.noreply.github.com>
2022-06-15 00:10:28 +0530

    modified: to solve the problem using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,27 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
-        int isPartTime = 1;
-        int isFullTime = 2;
-        int empRatePerHour = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static void main(String[] ags) {
+
         int empHours = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == isFullTime)
-            empHours = 8;
-        else if (empCheck == isPartTime)
-            empHours = 4;
-        else
-            empHours = 0;
-        empWage = empHours * empRatePerHour;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+
+            case (IS_FULL_TIME):
+                empHours = 8;
+                break;
+            case (IS_PART_TIME):
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
+        }
+        empWage = empHours * EMP_RATE_PER_HOUR;
         System.out.println("Daily employee wage : "+empWage);
     }
 }

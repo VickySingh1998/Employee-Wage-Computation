830df5b43fcecd874ca7c64a85d14be2062e4f2b
VickySingh1998 <104664359+VickySingh1998@users.noreply.github.com>
2022-06-14 19:53:22 +0530

    added: statement to check employee attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,11 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
+        int isPresent=1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == isPresent)
+            System.out.println("The Employee is Present");
+        else
+            System.out.println("The Employee is Absent");
     }
 }

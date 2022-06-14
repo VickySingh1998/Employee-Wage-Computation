3b5301cdc66c1fe8504fb40a929e324d22a235d6
VickySingh1998 <104664359+VickySingh1998@users.noreply.github.com>
2022-06-14 23:28:13 +0530

    added: part time employee and calculated wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,13 +2,17 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int isFullTime = 1;
+        int isPartTime = 1;
+        int isFullTime = 2;
         int empRatePerHour = 20;
         int empHours = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == isFullTime)
             empHours = 8;
+        else if (empCheck == isPartTime)
+            empHours = 4;
         else
             empHours = 0;
         empWage = empHours * empRatePerHour;
